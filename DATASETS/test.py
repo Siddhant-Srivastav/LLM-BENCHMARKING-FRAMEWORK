c5a52609f33e96c3de312ac48fda7cabe1b6005d
Siddhant Srivastav <rohitshiriwastv819@gmail.com>
2026-03-29 14:54:38 +0530

    Debug all issues

--- DATASETS/test.py
+++
@@ -1,4 +0,0 @@
-p.bar(models,scores)
-p.xticks(rotation=10)
-p.title("Qualitative Evaluation Using LLM-Based Judges")
-p.show()
--- response_length_metric.py
+++
@@ -1,4 +0,0 @@
-from base_metric import BaseMetric
-class ResponseLengthMetric(BaseMetric):
-    def compute(self,response):
-        return len(response)
